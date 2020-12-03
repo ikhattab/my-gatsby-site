e65946e1156bf94ea0f3f7648338200a1fc22742
Ihab Khattab <ihab@hey.com>
2020-12-04 00:03:04 +0200

    💄add marinda recipes theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,15 @@
 module.exports = {
-    plugins: [
-
-    ]
-}
+  plugins: [
+    {
+      resolve: "@marinda/gatsby-theme-recipes",
+      options: {
+        // your site title
+        title: "Classic Recipes",
+        // your site short title
+        shortTitle: "C-Recipes",
+        // the path to your icon file
+        iconPath: "./src/images/favicon.png",
+      },
+    },
+  ],
+};
